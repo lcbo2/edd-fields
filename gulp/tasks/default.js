c80d4a918fe98b47028103f6ce2e82d7273a4a0c
Eric Defore <eric.defore@gmail.com>
2016-07-22 10:48:38 -0400

    Ability to Process individual TinyMCE JavaScript Plugins via Gulp. Referencing #10

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,6 +1,6 @@
-var gulp          = require('gulp');
-var sequence      = require('run-sequence');
+var gulp          = require( 'gulp' );
+var sequence      = require( 'run-sequence' );
 
-gulp.task('default', function(done) {
-    sequence('sass', 'uglify', 'watch', done);
-});
+gulp.task( 'default', function( done ) {
+    sequence( 'sass', 'uglify', 'watch', done );
+} );
