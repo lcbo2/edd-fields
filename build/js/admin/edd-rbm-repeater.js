2b6ec6279c51be4ac2941085e243d31ff4f3a0b9
Eric Defore <eric.defore@gmail.com>
2017-06-12 13:40:36 -0400

    Same EDD Chosen fix as Slack. Closes #67

--- build/js/admin/edd-rbm-repeater.js
+++ build/js/admin/edd-rbm-repeater.js
@@ -75,7 +75,7 @@ function init_edd_rbm_repeater_required_fields(modal) {
         }
 
         // Fix Tab Ordering Bug
-        if ( jQuery(field).hasClass('edd-select-chosen') ) {
+        if ( jQuery(field).hasClass('edd-fields-chosen') ) {
 
             // Ensure the Chosen Container has been built
             jQuery(field).chosen();
