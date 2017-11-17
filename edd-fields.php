4448e331f74c83b00d97dc3e773d576e75d6d2ac
Eric Defore <eric.defore@gmail.com>
2017-11-17 12:20:56 -0500

    v1.0.0

--- edd-fields.php
+++ edd-fields.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Digital Downloads - Fields
  * Plugin URL: https://easydigitaldownloads.com/downloads/fields
  * Description: Easily create custom attributes or meta for your Downloads
- * Version: 1.0.0b2
+ * Version: 1.0.0
  * Text Domain: edd-fields
  * Author: Real Big Plugins
  * Author URI: https://realbigplugins.com
